969ea11d45932b316e2b926b8cf5f908aefd784b
Papalitas09 <miaw.raja@gmail.com>
2026-03-01 00:20:32 +0700

    nanti lagi

---
+++ resources/views/login.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.base')
+@section('title', 'login')
+@section('content')
+<form action="">
+    <label for="" class="bg-amber-300">login</label>
+    <input type="text">
+</form>
+
+@endsection
